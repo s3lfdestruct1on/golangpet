5d8bae4cf6de90d94052f2d5e365085bf2925248
Popov Anton <smoke2.nvkz@gmail.com>
2025-02-03 18:55:49 +0700

    added storage

--- storage-service/internal/storage/storage.go
+++ storage-service/internal/storage/storage.go
@@ -1,2 +1,16 @@
 package storage
 
+type User struct{
+
+	ID uint `json:"id"`
+	username string `json:"username"`
+	password string `json:"password"`
+}
+
+func (u *User)Username() string{
+	return u.username
+}
+
+func (u *User) Password() string{
+	return u.password
+}
